6563a70b24d85b8889b7539f9e3e3ffb5538f2aa
Leandro Possanzini <leandropossanzini@gmail.com>
2022-07-14 10:22:35 -0300

    Rutas protegidas

--- middlewares/rol.js
+++ middlewares/rol.js
@@ -1,12 +1,20 @@
 const { handleHttpError } = require("../utils/handleError");
 
-// Array con los roles permitidos
-const checkRol = (rol) =>(res, req, next) =>{
+// Array con los roles permitidos ["user", "admin", "manager"] 3 tipos
+const checkRol = (roles) =>(req, res, next) =>{
     try{
-        const {user} = req;
+        const { user } = req;
+        const rolesByUser = user.role;  // todo usuario que se registra obtiene ["user"]
+        
+        // ME responde con un true o false y comparo si dentro del array tengo los permisos
+        const checkValueRol = roles.some((rolSingle) => rolesByUser.includes(rolSingle))
+        if(!checkValueRol){
+            handleHttpError(res, "USER_NOT_PERMISSIONS", 403)
+            return 
+        }
         next()
     }catch (e){
-        handleHttpError(res, "ROL_NO_PERMITIDO")
+        handleHttpError(res, "ERROR_PERMISSIONS", 403)
     }
 }
 
